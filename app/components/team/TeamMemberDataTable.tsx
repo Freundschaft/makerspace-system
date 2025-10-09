c3a4a6d2f3d8de252810270c258482089864a186
Qiong Wu <qiong.wu@gfnork.de>
2025-10-10 00:41:00 +0200

    fix prisma import

--- app/components/team/TeamMemberDataTable.tsx
+++ app/components/team/TeamMemberDataTable.tsx
@@ -8,7 +8,7 @@ import {
   TableRow,
 } from "@/components/ui/table"
 import { Button } from "@/components/ui/button"
-import { TeamMember } from "@prisma/client"
+import { TeamMember } from "@/generated/prisma"
 import { format } from "date-fns"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Badge } from "@/components/ui/badge"
